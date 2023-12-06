c988bdf64889469c018c0d173ecfcf8bab1515aa
Yaser Mowlawizadah <ym23meq@nv00444.muc.navvis.com>
2023-12-06 17:24:35 +0100

    initiation

--- DES/C++/Scratch/mycpp.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    // Output "Hello, World!" to the console
-    std::cout << "Hello, World!" << std::endl;
-
-    // Return 0 to indicate successful completion
-    return 0;
-}
