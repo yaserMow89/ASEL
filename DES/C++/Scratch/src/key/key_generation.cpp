b1c08423cb66b3b304fd35abd942d870db46977d
Yaser Mowlawizadah <ym23meq@nv00444.muc.navvis.com>
2023-12-09 15:48:45 +0100

    Key DES

--- DES/C++/Scratch/src/key/key_generation.cpp
+++ DES/C++/Scratch/src/key/key_generation.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <random>
 #include <bitset>
+#include <string>
+
+using namespace std; 
 
 std::bitset<64> genKey() {
    const int keySize = 64;
@@ -27,75 +30,72 @@ std::bitset<64> genKey() {
 }
 
 
-void generateSubkeys(const std::bitset<64>& key, std::bitset<48> subkeys[16]) {
-        // Permutation choice 1 (PC-1)
-    const int pc1[56] = { 57, 49, 41, 33, 25, 17, 9,
-                          1, 58, 50, 42, 34, 26, 18,
-                          10, 2, 59, 51, 43, 35, 27,
-                          19, 11, 3, 60, 52, 44, 36,
-                          63, 55, 47, 39, 31, 23, 15,
-                          7, 62, 54, 46, 38, 30, 22,
-                          14, 6, 61, 53, 45, 37, 29,
-                          21, 13, 5, 28, 20, 12, 4 };
+void generateSubkeys(const std::string& key, std::string subkeys[16]) {
+    // Permutation choice 1 (PC-1)
+    const int pc1[56] = { 56, 48, 40, 32, 24, 16, 8,
+                          0, 57, 49, 41, 33, 25, 17,
+                          9, 1, 58, 50, 42, 34, 26,
+                          18, 10, 2, 59, 51, 43, 35,
+                          62, 54, 46, 38, 30, 22, 14,
+                          6, 61, 53, 45, 37, 29, 21,
+                          13, 5, 60, 52, 44, 36, 28,
+                          20, 12, 4, 27, 19, 11, 3 };
 
     // Permutation choice 2 (PC-2)
-    const int pc2[48] = { 14, 17, 11, 24, 1, 5,
-                          3, 28, 15, 6, 21, 10,
-                          23, 19, 12, 4, 26, 8,
-                          16, 7, 27, 20, 13, 2,
-                          41, 52, 31, 37, 47, 55,
-                          30, 40, 51, 45, 33, 48,
-                          44, 49, 39, 56, 34, 53,
-                          46, 42, 50, 36, 29, 32 };
+    const int pc2[48] = { 13, 16, 10, 23, 0, 4,
+                          2, 27, 14, 5, 20, 9,
+                          22, 18, 11, 3, 25, 7,
+                          15, 6, 26, 19, 12, 1,
+                          40, 51, 30, 36, 46, 54,
+                          29, 39, 50, 44, 32, 47,
+                          43, 48, 38, 55, 33, 52,
+                          45, 41, 49, 35, 28, 31 };
 
     // Apply PC-1
-    std::bitset<56> permutedKey;
+    std::string permutedKey(56, '0');  // Initialize with 56 '0' characters
     for (int i = 0; i < 56; ++i) {
-        permutedKey[i] = key[pc1[i] - 1];
+        permutedKey[i] = key[pc1[i]];
     }
 
+    std::cout << "Permuted Key (After PC-1): " << permutedKey << std::endl;
+
     // Split the permuted key into two halves
-    std::bitset<28> leftHalf, rightHalf;
-    for (int i = 0; i < 28; ++i) {
-        leftHalf[i] = permutedKey[i];
-        rightHalf[i] = permutedKey[i + 28];
-    }
+    std::string leftHalf = permutedKey.substr(0, 28);
+    std::string rightHalf = permutedKey.substr(28);
+  std::cout << leftHalf << std::endl;
+  std::cout << rightHalf << std::endl;
 
     // Generate subkeys
     for (int round = 0; round < 16; ++round) {
         // Perform circular left shift
-        leftHalf = (leftHalf << 1) | (leftHalf >> 27);
-        rightHalf = (rightHalf << 1) | (rightHalf >> 27);
+        leftHalf = leftHalf.substr(1) + leftHalf[0];
+        rightHalf = rightHalf.substr(1) + rightHalf[0];
 
         // Combine halves
-        std::bitset<56> roundKey;
-        for (int i = 0; i < 28; ++i) {
-            roundKey[i] = leftHalf[i];
-            roundKey[i + 28] = rightHalf[i];
-        }
+        std::string roundKey = leftHalf + rightHalf;
 
         // Apply PC-2 to generate the subkey
+        subkeys[round].resize(48); // Resize to match the size of the subkey
         for (int i = 0; i < 48; ++i) {
-            subkeys[round][i] = roundKey[pc2[i] - 1];
+            subkeys[round][i] = roundKey[pc2[i]];
         }
     }
 }
 
 int main() {
-    // Generate a 64-bit key
-    std::bitset<64> key = genKey();
+    // Manually define a 64-bit key for testing
+    std::string key = "0001001100110100010101110111100110011011101111001101111111110001";
 
-    // Array to store 16 subkeys, each 48 bits long
-    std::bitset<48> subkeys[16];
+    // Array to store 16 subkeys, each as a string
+    std::string subkeys[16];
 
     // Generate subkeys
     generateSubkeys(key, subkeys);
 
     // Display the generated subkeys
     for (int i = 0; i < 16; ++i) {
-        std::cout << "Subkey " << i + 1 << ": " << subkeys[i] << std::endl;
+        std::cout << "K" << i + 1 << " = " << subkeys[i] << std::endl;
     }
 
     return 0;
 }
-
